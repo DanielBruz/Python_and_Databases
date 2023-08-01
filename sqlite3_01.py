42bbb56dc877b02b15ef55ddd7eae2a1403539e4
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-08-01 22:54:44 +0200

    sqlite3_01

--- sqlite3_01.py
+++ sqlite3_01.py
@@ -3,3 +3,7 @@ https://docs.python.org/3/library/sqlite3.html
 """
 import sqlite3
 
+con = sqlite3.connect("tutorial.db")  # connection to the database (implicitly creating it if it does not exist)
+cur = con.cursor()  # vytvoření kurzoru pro práci s databází
+# create a database table movie with columns for title, release year, and review score:
+cur.execute("CREATE TABLE movie(title, year, score)")
